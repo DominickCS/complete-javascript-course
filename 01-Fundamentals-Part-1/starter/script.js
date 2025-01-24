347665cd12de4e96f2484e830c914a164833c8e5
Dominick Smith <dominick.smith938@gmail.com>
2025-01-24 13:57:56 -0600

    completed Data Types lecture

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,15 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
+// Log the value 'Dominick' to the console.
 console.log('Dominick');
+// Log the value 23 to the console.
 console.log(23);
 
+// Variable definition.
 let firstName = 'Dominick';
 
+// Log the value of firstName to the console.
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
@@ -16,4 +20,29 @@ let myFirstJob = 'Whataburger';
 let myCurrentJob = 'Data Center Technician';
 
 console.log(myFirstJob);
+*/
 
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// Logging the data types of different data types
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Dominick");
+
+// Demo of dynamic typing, reassigning a variable to a new value
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+// The declaration of an empty variable sets the value and type to undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2025;
+console.log(year);       
+console.log(typeof year);
+
+// Similarly to an empty string, type of null SHOULD BE null but due to legacy support, null is an object
+console.log(typeof null);
