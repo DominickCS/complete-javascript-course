37e101be2f306dad9221eaaff97ea093c92ecb5f
Dominick Smith <dominick.smith938@gmail.com>
2025-01-24 13:22:10 -0600

    added assignments dir and added respective files

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') alert("JavaScript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Dominick');
+console.log(23);
+
+let firstName = 'Dominick';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.14159;
+
+let myFirstJob = 'Whataburger';
+let myCurrentJob = 'Data Center Technician';
+
+console.log(myFirstJob);
+
