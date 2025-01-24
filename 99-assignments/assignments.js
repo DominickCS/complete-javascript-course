1590492d3f3b341e9e1b668b410c37a9d86cc845
Dominick Smith <dominick.smith938@gmail.com>
2025-01-24 14:04:53 -0600

    completed data types assignment

--- 99-assignments/assignments.js
+++ 99-assignments/assignments.js
@@ -1,7 +1,18 @@
+// Values and Variables Assignment
 let country = "United States of America";
 let continent = "North America";
 let population = 343600000;
 
-console.log(country);
 console.log(continent);
+console.log(country);
 console.log(population);
+
+
+// Data Types Assignment
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
