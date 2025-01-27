5e7dde7c27f31190d3aa3bba307c9f8c916b1b09
DominickCS <Dominick.Smith938@gmail.com>
2025-01-26 18:56:46 -0600

    completed type conversion type coercion assignment

--- 99-assignments/assignments.js
+++ 99-assignments/assignments.js
@@ -47,9 +47,16 @@ if(population > averagePopulation) {
   console.log(`${country}'s population is ${population - averagePopulation} people above average`);
 }
 population = 13000000;
-if(population > averagePopulation) {                                                              
+if(population > averagePopulation) {
   console.log(`${country}'s population is ${population - averagePopulation} people above average`);
 } else {
   console.log(`${country}'s population is ${averagePopulation - population} people below average`);
-}                                                                                                 
+}
 
+// Type Conversion and Coercion Assignment
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> 617
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
