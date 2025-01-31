8b19ab8f359aab5cabeb2a6ec07386ec3d407fc3
DominickCS <Dominick.Smith938@gmail.com>
2025-01-31 15:10:05 -0600

    completed Introduction to Objects Assignment

--- 99-assignments/assignments.js
+++ 99-assignments/assignments.js
@@ -172,3 +172,13 @@ console.log(populations.length === 4);
 
 const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])];
 console.log(percentages);
+
+// Introduction to Objects Assignment
+
+const myCountry = {
+  country: `United States of America`,
+  capital: `Washington DC`,
+  language: `English`,
+  population: `${populations[0]}`,
+  neigbors: [`Canada`, `Mexico`]
+}
