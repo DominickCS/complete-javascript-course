71b1d15771c6264d78e13548b487b54a2b22aa79
Dominick Smith <dominick.smith938@gmail.com>
2025-02-12 15:53:41 -0600

    completed using google, stackoverflow and MDN lecture

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,7 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const temperatures = [3,-2,-6,-1, `error`, 9, 13, 17, 15, 14, 9, 5];
+const temperaturesOne = [3,-2,-6,-1, `error`, 9, 13, 17, 15, 14, 9, 5];
+const temperaturesTwo = [4,2,7,34, `error`, 34 -63,-9385, 4, 2304];
 
 // 1) Understanding the problem
 // What is temp amplitude?
@@ -16,12 +17,36 @@ const temperatures = [3,-2,-6,-1, `error`, 9, 13, 17, 15, 14, 9, 5];
 // Find min value in the temperatures array
 // Subtract min from max (amplitude) and return it
 
-const calcTempAmplitude = function(temps) {
-  let maxTemp = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    if (temps[i] > maxTemp) maxTemp = temps[i];
+const calcTempAmplitude = function(tempsArray1, tempsArray2) {
+  const finalTemps = tempsArray1.concat(tempsArray2);
+  let maxTemp = finalTemps[0];
+  let minTemp = finalTemps[0];
+
+
+  for (let i = 0; i < finalTemps.length; i++) {
+    if(typeof finalTemps[i] === 'number') {
+      if (finalTemps[i] > maxTemp) maxTemp = finalTemps[i];
+      if (finalTemps[i] < minTemp) minTemp = finalTemps[i];
+    } else {
+      console.log(`${finalTemps[i]} is not a number!
+this data is located at array index ${i}`);
+      continue;
     }
-  console.log(maxTemp);
+  }
+
+  console.log(`The minimum temperature in the array was ${minTemp},
+and the maximum temperature was ${maxTemp}.`)
+
+  let amplitude = maxTemp - minTemp;
+  return(amplitude);
   };
 
-calcTempAmplitude([3, 4, 5, 6, 7, 8, 192]);
+
+// calcTempAmplitude([3, 4, 5, 6, 7, 8, 192]);
+console.log(calcTempAmplitude(temperaturesOne, temperaturesTwo));
+
+
+// Problem 2:
+// Function shouuld now receive two arrays of temperatures
+// With two arrays do we need to implement two functions?
+// How to merger two arrays
