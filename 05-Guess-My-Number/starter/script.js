3cb1401774a0e70663487bcb3662e745aac9c7e8
Dominick Smith <dominick.smith938@gmail.com>
2025-02-14 13:52:15 -0600

    completed handling click events lecture

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,6 +2,7 @@
 
 'testing';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -13,3 +14,16 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  document.querySelector('.message').textContent = 'Correct Number!';
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      `You didn't enter a number :( `;
+  }
+});
