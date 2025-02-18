ce9540a146b5204310b0bf84ad44ed34ab973864
Dominick Smith <dominick.smith938@gmail.com>
2025-02-18 11:11:06 -0600

    started modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
+});
