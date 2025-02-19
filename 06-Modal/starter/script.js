dbc0ff77bf6b0fb4f6c40be945c9ccb9ea2991e7
DominickCS <Dominick.Smith938@gmail.com>
2025-02-18 18:27:06 -0600

    refactored modal functions to align with DRY

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,14 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', function () {
-    modal.style.display = 'block';
-    overlay.style.display = 'block';
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.style.display = 'none';
-  overlay.style.display = 'none';
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
